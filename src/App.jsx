98a10592b837a8e5e6987ae1b4049cc66a1853ae
Sam <samuel.fitz@btinternet.com>
2023-11-11 21:12:18 +0000

    feat: reset files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <form action="">
+        <input type="text" name="name" id="line1" placeholder="Line 1" />
+        <input type="text" name="name" id="line2" placeholder="Line 2" />
+        <input type="text" name="name" id="line3" placeholder="Line 3" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  )
+}
+
+export default App
